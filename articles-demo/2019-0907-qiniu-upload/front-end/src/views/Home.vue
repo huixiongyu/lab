3ae8ed4765ead977e6c40a0b6fb6a28a1b0dd58a
huixiongyu <huixiongyu@gmail.com>
2019-09-10 08:03:03 +0800

    upload with name success

--- articles-demo/2019-0907-qiniu-upload/front-end/src/views/Home.vue
+++ articles-demo/2019-0907-qiniu-upload/front-end/src/views/Home.vue
@@ -62,6 +62,5 @@ export default {
   .directly-upload{
     width: 600px;
     height: 400px;
-    border: 1px solid red;
   }
 </style>
--- articles-demo/2019-0907-qiniu-upload/front-end/src/views/WithName.vue
+++ articles-demo/2019-0907-qiniu-upload/front-end/src/views/WithName.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="about">
-    <Upload
-      ref="upload"
-      multiple
-      type="drag"
-      :before-upload="handleBeforeUpload"
-      :on-success="handleSuccess"
-      :format="['jpg', 'jpeg', 'png']"
-      :max-size="4096"
-      :data="{token: qiniuToken, key: keyName}"
-      :action="postURL">
-      <div style="padding: 20px 0">
-          <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-          <p>点击或者拖拽文件到此处上传</p>
-      </div>
-    </Upload>
+  <div class="with-name">
+    <div class="directly-upload">
+      <Upload
+        ref="upload"
+        multiple
+        type="drag"
+        :before-upload="handleBeforeUpload"
+        :on-success="handleSuccess"
+        :format="['jpg', 'jpeg', 'png']"
+        :max-size="4096"
+        :data="{token: qiniuToken, key: keyName}"
+        :action="postURL">
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>点击或者拖拽文件到此处上传</p>
+        </div>
+      </Upload>
+    </div>
   </div>
 </template>
 <script>
@@ -25,7 +27,7 @@ export default {
     return {
       postURL: 'http://upload-z2.qiniup.com',
       qiniuToken: '',
-      keyName: 'dingxiangjie.jpg',
+      keyName: '',
     };
   },
   methods: {
@@ -49,24 +51,31 @@ export default {
     // },
     // 修改文件的名字后上传，可以避免重复
     async handleBeforeUpload(file) {
+      console.log(file);
+      const suffixList = file.name.split('.');
+      const baseName = suffixList[0];
+      const suffix = suffixList[1];
+      const fileType = file.type;
       const timeStamp = new Date().getTime();
-      const newfile = new File([file], `${timeStamp}.jpg`, { type: 'image/jpeg' });
+      const newName = `${timeStamp}_${baseName}.${suffix}`;
+      const newfile = new File([file], newName, { type: fileType });
+      this.keyName = newName;
       console.log(newfile);
-      // await this.$http.get('/qiniu/token/name', {
-      //   params: {
-      //     name: 'dingxiangjie.jpg',
-      //   },
-      // }).then((res) => {
-      //   console.log(res.data.token);
-      //   this.qiniuToken = res.data.token.trim();
-      // })
-      //   .catch((err) => {
-      //     this.$Notice.error({
-      //       title: '错误',
-      //       desc: err ? '' : '上传失败',
-      //     });
-      //   });
-      // console.log('我等到了...');
+      await this.$http.get('/qiniu/token/name', {
+        params: {
+          name: newName,
+        },
+      }).then((res) => {
+        console.log(res.data.token);
+        this.qiniuToken = res.data.token.trim();
+      })
+        .catch((err) => {
+          this.$Notice.error({
+            title: '错误',
+            desc: err ? '' : '上传失败',
+          });
+        });
+      console.log('我等到了...');
     },
     handleSuccess(res) {
       console.log(res);
@@ -74,3 +83,15 @@ export default {
   },
 };
 </script>
+<style lang="less" scoped>
+  .with-name{
+    width: 100vw;
+    height: 900px;
+    display: flex;
+    justify-content: center;
+  }
+  .directly-upload{
+    width: 600px;
+    height: 400px;
+  }
+</style>
