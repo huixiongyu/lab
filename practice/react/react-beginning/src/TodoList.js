38117912dbeece917bb54ba36819580a04f72903
huixiongyu <huixiongyu@gmail.com>
2020-03-24 00:29:41 +0800

    redux add

--- practice/react/react-beginning/src/TodoList.js
+++ practice/react/react-beginning/src/TodoList.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input , Button, List } from 'antd';
+import axios from 'axios';
+import store from './store';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+  }
   state = {  }
   render() { 
     return ( 
+      <div style={{margin:'10px'}}>
         <div>
-          hi
+            <Input placeholder={this.state.inputValue} style={{ width:'250px', marginRight:'10px'}}/>
+            <Button type="primary">增加</Button>
         </div>
+        <div style={{margin:'10px',width:'300px'}}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => (
+              <List.Item>
+                {item}
+              </List.Item>
+              )}
+            />
+        </div>
+      </div>
      );
+  }
+
+  componentDidMount() {
+    console.log('hahahah');
+    axios.get('http://rap2.taobao.org:38080/app/mock/data/1501675').then((res) => {
+      console.log(res);
+    });
   }
 }
  
