1cc7af530ba78ae36d6ee0e80411972a4fe2a08f
huixiongyu <huixiongyu@gmail.com>
2020-04-24 17:45:55 +0800

    finish react-practice

--- practice/react/react-practice/src/AddNumber.js
+++ practice/react/react-practice/src/AddNumber.js
@@ -6,7 +6,9 @@ class AddNumber extends Component {
     }
     constructor(props) {
         super(props)
+        console.log('1.构造函数')
         this.state = {number: 1}
+        console.log('2.设置状态机')
         this.addNum = this.addNum.bind(this)
     }
     addNum() {
@@ -14,7 +16,11 @@ class AddNumber extends Component {
             number: this.state.number + 1
         })
     }
+    componentWillMount() {
+      console.log('3. componentWillMount 渲染前调用');
+    }
     render() {
+        console.log('4. 组件进行渲染了呢')
         return (
             <div>
                 <div>Hello  {this.props.name}</div>
@@ -22,6 +28,9 @@ class AddNumber extends Component {
                 <button onClick={this.addNum}>增加</button>
             </div>
         )
+    }
+    componentDidMount() {
+        console.log('5. 组件渲染完了呢')
     }
 }
 
